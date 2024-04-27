8cd6eda7c454bb074d90b1ab7d2cab9c7e9af403
Liubo Ren <133237884+lewiswlker@users.noreply.github.com>
2024-04-27 18:04:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,9 +3,6 @@ from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer
 model = Unet(
     dim = 96,
     dim_mults = (1, 2, 4, 8),
-    self_condition = False,
-    learned_variance = False,
-    learned_sinusoidal_cond = False,
     flash_attn = True
 )
 
@@ -13,7 +10,7 @@ diffusion = GaussianDiffusion(
     model,
     image_size = 64,
     timesteps = 1000,           # number of steps
-    sampling_timesteps = 250,
+    sampling_timesteps = 250
 )
 
 trainer = Trainer(
@@ -27,10 +24,10 @@ trainer = Trainer(
     amp = True,                       # turn on mixed precision
     calculate_fid = True,             # whether to calculate fid during training
     save_and_sample_every = 5000,
-    results_folder = '/root/autodl-tmp/results_navigate_best',
+    results_folder = '/root/autodl-tmp/results',
     save_best_and_latest_only = True,
     num_fid_samples = 2000,
-    do_load = True,
+    do_load = False,
     load_milestone='latest'
 )
 
